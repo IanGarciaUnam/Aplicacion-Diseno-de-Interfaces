a3357b739cb21cdd55c9e8b9a38233fbf406fc58
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2022-05-09 19:34:11 -0500

    Práctica API Backend

---
+++ consume_api/main_app/views.py
@@ -0,0 +1,10 @@
+import requests
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+  # Obtenemos la lista de cosas por hacer
+  response = requests.get('https://jsonplaceholder.typicode.com/todos/')
+  # Tranformamos la respuesta en un objeto JSON
+  todos = response.json()
+  return render(request, "main_app/home.html", {"todos": todos})
