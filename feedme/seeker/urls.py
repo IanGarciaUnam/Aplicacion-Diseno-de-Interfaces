373d018f70d9f2e2e81c4b4024dfd4b985165cf8
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2022-05-16 21:40:18 -0500

    trying to add new image

--- feedme/seeker/urls.py
+++ feedme/seeker/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .views import HomeView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', HomeView.as_view(), name='index')
 ]
--- feedme/seeker/views.py
+++ feedme/seeker/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+  template_name='index.html'
+  
 def index(request):
   return render(request, 'seeker/index.html', {})
   # return HttpResponse("Hello, world. You're at the polls index.")
