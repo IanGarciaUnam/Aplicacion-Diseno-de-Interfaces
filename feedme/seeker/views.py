87b01bbe8a1e0ac2d6e9b16cdf8e8b5833db7471
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2022-05-22 23:26:49 -0500

    Implementando vista de login

--- feedme/seeker/views.py
+++ feedme/seeker/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return render(request, 'seeker/index.html', {})
-  # return HttpResponse("Hello, world. You're at the polls index.")
+
+def login(request):
+  return render(request, 'seeker/login.html', {})
+
+def register(request):
+  return render(request, 'seeker/register.html', {})
