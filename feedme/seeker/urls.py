f9269b747f6371b4828c255d245a03d420e0a8d5
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2022-05-24 21:17:25 -0500

    adding pag-resultados:usuario-no-logueado [almost done]

--- feedme/seeker/urls.py
+++ feedme/seeker/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('feedme', views.index, name='index'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-    path('reset-pass', views.reset_pass, name='reset-pass')
-]
+    path('reset-pass', views.reset_pass, name='reset-pass'),
+    path('results-nl', views.results_no_login, name='results-nl')
+    ]
