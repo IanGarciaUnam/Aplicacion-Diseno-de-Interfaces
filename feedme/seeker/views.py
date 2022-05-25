d14a9eafabcb8a0298a0e280566cd9f91f3dba79
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2022-05-24 23:40:20 -0500

    Pull from origin main

--- feedme/seeker/views.py
+++ feedme/seeker/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+  template_name='index.html'
+  
 def index(request):
   return render(request, 'seeker/index.html', {})
-  # return HttpResponse("Hello, world. You're at the polls index.")
+
+def login(request):
+  return render(request, 'seeker/login.html', {})
+
+def register(request):
+  return render(request, 'seeker/register.html', {})
+
+def reset_pass(request):
+  return render(request, 'seeker/reset-pass.html', 
+                
+def results_no_login(request):
+  return render(request, 'seeker/results-nl.html', {})
