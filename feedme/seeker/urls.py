00ed5dcc7cd0ac19aee98dec39b1df9c2ddf04b3
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2022-05-24 23:32:14 -0500

    Merge from branch Ian

--- feedme/seeker/urls.py
+++ feedme/seeker/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
+from .views import HomeView
 from . import views
 
 urlpatterns = [
     path('feedme', views.index, name='index'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
-]
+    path('reset-pass', views.reset_pass, name='reset-pass'),
+    path('results-nl', views.results_no_login, name='results-nl')
+    ]
--- feedme/seeker/views.py
+++ feedme/seeker/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomeView(TemplateView):
+  template_name='index.html'
+  
 def index(request):
   return render(request, 'seeker/index.html', {})
 
@@ -10,3 +14,9 @@ def login(request):
 
 def register(request):
   return render(request, 'seeker/register.html', {})
+
+def reset_pass(request):
+  return render(request, 'seeker/reset-pass.html', {})
+
+def results_no_login(request):
+  return render(request, 'seeker/results-nl.html', {})
