546ad1ea79ba7635f36a06fc67f9dcd1e0d20a5e
Luis Alberto Hernández Aguilar <luis_hernandez@ciencias.unam.mx>
2022-06-14 22:45:07 -0500

    Implementando registro, inicio y cierre de sesión

--- feedme/seeker/urls.py
+++ feedme/seeker/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('feedme', views.index, name='index'),
-    path('login', views.login, name='login'),
     path('register', views.register, name='register'),
+    path('login', views.loginPage, name='login'),
+    path('logout', views.logoutUser, name='logout'),
     path('reset-pass', views.reset_pass, name='reset-pass'),
     path('results-nl', views.results_no_login, name='results-nl'),
     path('receta', views.receta, name='receta')
