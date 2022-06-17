2f5989ff8a539dd295cb73122dd9e9543a7ac0f9
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2022-06-17 11:19:07 -0500

    main screen appearing| after loign screen appearing

--- feedme/seeker/views.py
+++ feedme/seeker/views.py
@@ -16,13 +16,16 @@ class HomeView(TemplateView):
     template_name = 'index.html'
 
 
+def index_login(request):
+    return render(request, 'seeker/index_login.html', {})
+
 def index(request):
     return render(request, 'seeker/index.html', {})
 
 
 def loginPage(request):
     if request.user.is_authenticated:
-        return redirect('index')
+        return redirect('index_login')
     else:
         if request.method == 'POST':
             username = request.POST['username']
@@ -32,7 +35,7 @@ def loginPage(request):
 
             if user is not None:
                 login(request, user)
-                return redirect('index')
+                return redirect('index_login')
 
         context = {}
         return render(request, 'seeker/login.html', context)
