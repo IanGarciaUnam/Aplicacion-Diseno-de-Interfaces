62144016f100ae909e32779a71f98dab9fe82577
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2022-06-20 01:35:58 -0500

    Filling recipes and ingredients

--- feedme/seeker/models.py
+++ feedme/seeker/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
 # Create your models here.
 
 class Alimento(models.Model):
@@ -9,20 +8,41 @@ class Alimento(models.Model):
     grasas = models.FloatField(null=True)
     proteina = models.FloatField(null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.nombre
 
 
 class Receta(models.Model):
     alimento = models.ManyToManyField(Alimento)
     nombre = models.CharField(max_length=200, null=True)
-    calorias = models.FloatField(null=True)
+    #calorias = models.FloatField(null=True)
     preparacion = models.TextField(null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True, null=True)
+    
+    @property
+    def calorias_totales(self):
+        suma_kcal=0
+        for al in self.alimento.all():
+            suma_kcal+=al.calorias
+        return suma_kcal
+    """
+    def save(self, *args, **kwargs):
+          self.calorias = self.get_calorias()
+          super(Receta, self).save(*args, **kwargs)
+    """
+    def __str__(self):
+        return self.nombre
 
 
 class Ingrediente(models.Model):
     receta = models.ManyToManyField(Receta)
     alimento = models.ManyToManyField(Alimento)
     porcion = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.alimento)+self.porcion
+
 
 
 class Usuario(models.Model):
@@ -41,3 +61,4 @@ class Tutor(models.Model):
 
     def __str__(self):
         return self.name
+
